2d34298252e218d83f53439a26ecbd0f7242da69
wailunlim <wailun@u.nus.edu>
2019-01-21 03:08:21 +0800

    refactor tests

--- test/site_pages_test.rb
+++ test/site_pages_test.rb
@@ -7,12 +7,16 @@ class PalindromeAppTest < Minitest::Test
     Sinatra::Application
   end
 
+  def setup
+    @base_title = "Learn Enough Ruby Sample App"
+  end
+
   def test_index
     get '/'
     assert last_response.ok?
     assert doc(last_response).at_css('h1')
     title_content = doc(last_response).at_css('title').content
-    assert_equal "Learn Enough Ruby Sample App | Home", title_content
+    assert_equal "#{@base_title} | Home", title_content
     assert doc(last_response).at_css('nav')
   end
 
@@ -21,7 +25,7 @@ class PalindromeAppTest < Minitest::Test
     assert last_response.ok?
     assert doc(last_response).at_css('h1')
     title_content = doc(last_response).at_css('title').content
-    assert_equal "Learn Enough Ruby Sample App | About", title_content
+    assert_equal "#{@base_title} | About", title_content
   end
 
   def test_palindrome
@@ -29,7 +33,6 @@ class PalindromeAppTest < Minitest::Test
     assert last_response.ok?
     assert doc(last_response).at_css('h1')
     title_content = doc(last_response).at_css('title').content
-    assert_equal "Learn Enough Ruby Sample App | Palindrome Detector",
-                 title_content
+    assert_equal "#{@base_title} | Palindrome Detector", title_content
   end
 end
